b8b7391ba6bf82cfc90d82781b294af5b046146b
Akshada <akshada1kusalkar@gmail.com>
2025-11-19 19:01:58 +0530

    adding123

--- python/app.py
+++ python/app.py
@@ -18,7 +18,8 @@ from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnableLambda
 from langchain_core.output_parsers import StrOutputParser
-import requests
+import requests 
+#########
 from symspellpy import SymSpell
 # from langchain.retrievers.multi_query import MultiQueryRetriever
 import httpx
