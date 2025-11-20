7dbed81fc95cd7571f3c596b58c0cd2abf2c7e8d
Animesh <dr.illuminati.06@gmail.com>
2025-11-20 11:41:41 +0530

    backend changes

--- backend/index.js
+++ backend/index.js
@@ -21,16 +21,7 @@ app.use(
 app.use(express.json());
 
 app.use("/documents", express.static(path.join(__dirname, "documents/")));
-app.use("/products", express.static(path.join(__dirname, "products/")));
-app.use("/sectors", express.static(path.join(__dirname, "sectors/")));
 app.use("/profile", express.static(path.join(__dirname, "profile/")));
-app.use("/uploads", express.static(path.join(__dirname, "uploads/")));
-app.use("/uploads/community", express.static(path.join(__dirname, "uploads/community")));
-
-app.use('/uploadFiles', express.static(path.join(__dirname, 'uploadFiles/')));
-app.use('/videoFiles', express.static(path.join(__dirname, 'videoFiles/')));
-
-// app.use('/uploadFiles', express.static(path.join(__dirname, 'uploadFiles')));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -38,14 +29,7 @@ app.use(express.json());
 app.use('/admin', require("./routes/admin/admin"))
 app.use('/support', require("./routes/admin/support"))
 app.use('/documents', require("./routes/admin/documents"))
-app.use('/product_service', require("./routes/admin/product_service"))
-app.use('/campaign', require("./routes/admin/campaign"))
-app.use('/sector', require("./routes/admin/sector"))
 app.use('/employee', require("./routes/admin/employee"))
-app.use('/contact', require("./routes/admin/contact_list"))
-app.use('/bots', require("./routes/admin/bots"))
-app.use('/template', require("./routes/admin/template"))
-app.use('/', require('./routes/admin/webhook'))
 
 app.use('/user', require("./routes/users/user"))
 app.use('/community', require("./routes/users/community"))
@@ -54,8 +38,10 @@ app.use('/chatbot', require("./routes/users/chatbot"))
 app.use('/ai', require('./routes/users/aiChat'));
 
 
+app.use('/meetings', require('./routes/admin/meetings'));
+
+
 const port = 7601;
 app.listen(port, () => {
     console.log(`http://localhost:${port}`)
-
 })
