94f71e60607813a8d7c89283222717ab71382afa
Akshada <akshada1kusalkar@gmail.com>
2025-11-25 16:53:00 +0530

    Clean commit without HF token

--- python/setup.py
+++ python/setup.py
@@ -10,15 +10,15 @@ import sys
 HUGGINGFACE_TOKEN = os.getenv('HUGGINGFACE_TOKEN')
 
 login(HUGGINGFACE_TOKEN) 
-
-# sentence_transformer = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
-# sentence_transformer.save('./models/all-MiniLM-L6-v2')
+print('login successful')
+sentence_transformer = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+sentence_transformer.save('/root/github/python_model/all-MiniLM-L6-v2')
 print("✔ successfully Download the senetence Transformer model............") 
 
 LLM_model = hf_hub_download(
     repo_id='Qwen/Qwen2.5-3B-Instruct-GGUF'  ,  
     filename='qwen2.5-3b-instruct-q4_k_m.gguf' , 
-    local_dir= '/root/github/chatbot_models'
+    local_dir= '/root/github/python_model'
     
 ) 
 
